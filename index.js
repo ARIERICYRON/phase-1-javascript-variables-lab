ef61e2b000a3494a71defd2c7b01d547fbc85243
arieri <ariericyron@gmail.com>
2022-06-06 15:44:24 +0300

    commit tree e2590ad4fa389ba4dfd0eb766b071dda093113c6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+mostProfitableNeighborhood='Chelsea';
+let mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan smith';
+let companyCeo = "Susan Smith";
